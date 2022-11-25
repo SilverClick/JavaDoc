a1df29bdf1a860d1cffbc106a3648866ef911972
Gonzalo Arca Iglesias <garcaiglesias@danielcastelao.org>
2022-11-25 09:54:41 +0100

    primer commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## JavaDOC
+
+[Referencia Javadoc]()
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Academica"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Persoal"}]
